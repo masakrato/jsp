573cfa811c9efd54320b59632a1998c65f2e951e
Mateusz Bieniek <mpower.sgs7@gmail.com>
2020-03-07 11:22:27 +0100

    zadanie 24

--- src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
@@ -10,27 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet"
-        , urlPatterns = {"/", "/hello"}
-        , initParams = {
-        @WebInitParam(name = "userId", value = "123456")
-        ,@WebInitParam(name = "localization", value = "Poland")
-})
+//@WebServlet(name = "HelloWorldServlet"
+//        , urlPatterns = {"/", "/hello"})
+
 public class HelloWorldServlet extends HttpServlet {
-
-    private String userId, localization;
-
-    @Override
-    public void init(ServletConfig config) {
-        userId = config.getInitParameter("userId");
-        localization = config.getInitParameter("localization");
-    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         final PrintWriter writer = response.getWriter();
         writer.println("Hello World");
-        writer.println("userId: " + userId);
-        writer.println("localization: " + localization);
     }
 }
